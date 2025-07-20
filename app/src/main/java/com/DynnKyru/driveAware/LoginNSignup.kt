63db40db95d5d56fd7ec849df6e20eb7ac069d91
DynnKyru <kylerydynn20@gmail.com>
2025-07-21 03:53:39 +0800

    just login fix

--- app/src/main/java/com/DynnKyru/driveAware/LoginNSignup.kt
+++ app/src/main/java/com/DynnKyru/driveAware/LoginNSignup.kt
@@ -33,18 +33,13 @@ class LoginNSignup : AppCompatActivity() {
         val showSignup = intent.getBooleanExtra("showSignup", false)
 
 
-        /*if (showSignup) {
+        if (showSignup) {
             setContentView(R.layout.signup)
             setupSignup()
         } else {
             setContentView(R.layout.login)
             setupLogin()
-        }*/
-
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-        finish()
-
+        }
     }
 
     private fun setupLogin() {

f5854209e6cba318998a18e873fd7b12fe2aff85
AntonTolentino <155000488+AntonTolentino@users.noreply.github.com>
2025-07-22 00:21:11 +0800

    basta ayon

--- app/src/main/java/com/DynnKyru/driveAware/LoginNSignup.kt
+++ app/src/main/java/com/DynnKyru/driveAware/LoginNSignup.kt
@@ -33,13 +33,17 @@ class LoginNSignup : AppCompatActivity() {
         val showSignup = intent.getBooleanExtra("showSignup", false)
 
 
-        if (showSignup) {
+        /*if (showSignup) {
             setContentView(R.layout.signup)
             setupSignup()
         } else {
             setContentView(R.layout.login)
             setupLogin()
-        }
+        }*/
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+
     }
 
     override fun onStart() {
